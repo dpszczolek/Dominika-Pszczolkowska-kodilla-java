2655b07481164b9126c7ab7f6a05e34d91095f14
dpszczolek <dpszczolek@gmail.com>
2018-01-26 21:49:39 +0100

    6.6

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/ForumStats.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/ForumStats.java
@@ -18,18 +18,28 @@ class ForumStats {
         int cC = statistics.commentsCount();
         return cC;
     }
+    public double avgPostsPerUser () {
+        double aPostPerUser = (double) numberOfPosts()/numberOfUsers();
+        return aPostPerUser;
+    }
+    public double avgCommentsPerUser () {
+        double aCommentsPerUser = (double) numberOfComments()/numberOfUsers();
+        return aCommentsPerUser;
+    }
+    public double avgCommentsPerPost () {
+        double aCommentsPerPost = (double) numberOfComments()/numberOfPosts();
+        return aCommentsPerPost;
+    }
     public void calculateAdvStatistics(Statistics statistics) {
-        double avgPostsPerUser = (double) numberOfPosts()/numberOfUsers();
-        double avgCommentsPerUser = (double) numberOfComments()/numberOfUsers();
-        double avgCommentsPerPost = (double) numberOfComments()/numberOfPosts();
+     //   ...
     }
 
    public void showStatistics () {
         System.out.println("Number of users is " + numberOfUsers());
         System.out.println("Number of posts is " + numberOfPosts());
         System.out.println("Number of comments is " + numberOfComments());
-        System.out.println("Average number of posts per user is " );
-      //  System.out.println("Average number of comments per user is " + calculateAdvStatistics().avgCommentsPerUser);
-      //  System.out.println("Average number of comments per post is " + calculateAdvStatistics().avgCommentsPerPost);
+        System.out.println("Average number of posts per user is " + avgPostsPerUser());
+        System.out.println("Average number of comments per user is " + avgCommentsPerUser());
+        System.out.println("Average number of comments per post is " + avgCommentsPerPost());
     }
 }
