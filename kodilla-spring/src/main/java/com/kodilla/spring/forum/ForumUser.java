f1cce91f516d45d98f2ba0783c423781da6fb114
dpszczolek <dpszczolek@gmail.com>
2018-02-17 08:27:37 +0100

    10.1

--- kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -1,4 +1,12 @@
 package com.kodilla.spring.forum;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ForumUser {
+
+    public String getUsername () {
+        return "John Doe";
+    }
+
 }
