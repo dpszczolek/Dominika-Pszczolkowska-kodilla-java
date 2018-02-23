6266e4ca2359da6f0d7c68be3fa733990214c6fe
dpszczolek <dpszczolek@gmail.com>
2018-02-23 13:44:43 +0100

    10.4 v2

--- kodilla-spring-web/src/main/java/com/kodilla/kodillaspringweb/KodillaSpringWebApplication.java
+++ kodilla-spring-web/src/main/java/com/kodilla/kodillaspringweb/KodillaSpringWebApplication.java
@@ -1,4 +1,4 @@
-package com.kodilla.spring.web;
+package com.kodilla.kodillaspringweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
